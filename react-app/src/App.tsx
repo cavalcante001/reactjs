685bcfa729c9c49ab37b41977e02e42e01b6b205
Paulo <paulocavalcante001@outlook.com>
2022-09-25 22:06:12 -0300

    Usando state

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const App = () => {
 
+  const [numero, setNumero] = useState(0);
+
   const handleClick = () => {
-    alert('Clicou!');
+    setNumero(2);
   }
 
   return (
     <div>
+      <p>O número é: {numero}</p>
       <button onClick={handleClick}>Clique aqui</button>
     </div>
   );
