a50555312e36ebea9c8ada2b744144b3c6c97676
Paulo <paulocavalcante001@outlook.com>
2022-09-25 21:31:50 -0300

    Utilizando props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,6 +3,9 @@ import { Header } from './components/Header';
 const App = () => {
   return (
     <div>
+      <Header title="Título 1"/>
+      <Header title="Título 2"/>
+      <Header title="Título 3"/>
       <Header />
       Olá mundo
     </div>
