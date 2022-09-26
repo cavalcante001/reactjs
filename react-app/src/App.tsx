e1f171f135e7856d7f32ec791257a2e1fa878857
Paulo <paulocavalcante001@outlook.com>
2022-09-25 22:29:02 -0300

    Exercicio: criando um formulário

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,44 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 const App = () => {
 
-  const [name, setName] = useState('Pedro');
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState(0);
 
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleName = (event: React.ChangeEvent<HTMLInputElement>)=> {
     setName(event.target.value);
   }
-  
+
+  const handleLastName = (event: React.ChangeEvent<HTMLInputElement>)=> {
+    setLastName(event.target.value);
+  }
+
+  const handleAge = (event: React.ChangeEvent<HTMLInputElement>)=> {
+    setAge(+event.target.value);
+  }
+
   return (
     <div>
-      Nome:
-      <input type="text" value={name} onChange={handleInput} />
-      <hr />
-      Seu nome é: {name}
+      <div>
+        Nome:
+        <input type="text" onChange={handleName}/>
+      </div>
+
+      <div>
+        Sobrenome:
+        <input type="text" onChange={handleLastName}/>
+      </div>
+
+      <div>
+        Idade:
+        <input type="text" onChange={handleAge} />
+      </div>
+
+      <hr/>
+
+      <p>Olá {`${name} ${lastName}`}, tudo bem?</p>
+      <p>Você tem {age} anos.</p>
     </div>
   );
 }
