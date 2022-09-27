95268a8c3222952261d914496ea0d456b6db8ba8
Paulo <paulocavalcante001@outlook.com>
2022-09-27 11:56:36 -0300

    Redirecionamento de rotas

--- react-app/src/pages/AboutItem.tsx
+++ react-app/src/pages/AboutItem.tsx
@@ -1,10 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export const AboutItem = () => {
     const params = useParams();
+    const navigate = useNavigate();
+
+    const handleBackButton = () => {
+        navigate(-1);
+    }
+
+    const handleHomeButton = () => {
+        let name: string = 'cilano';
+        navigate(`/sobre/${name}`);
+    }
+
     return (
         <div>
-           Página sobre {params.slug}
+           Página sobre {params.slug?.toUpperCase()} ({params.slug?.length} letras) 
+           <hr />
+           <button onClick={handleBackButton}>Voltar</button>
+           <button onClick={handleHomeButton}>Home</button>
         </div>
     );
 }
