19b9c7d1ae5f18ea425885dc749ea565c9c1c94e
Paulo <paulocavalcante001@outlook.com>
2022-10-07 17:04:51 -0300

    Requisição simples 2

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Category } from './types/Category'
 
 const App = () => {
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    handleLoadButton();
+  }, []);
 
   const handleLoadButton = () => {
     fetch('http://localhost.com:5003/categories')
@@ -18,6 +23,15 @@ const App = () => {
     <div>
       <button className="block bg-blue-400 p-2 rounded" onClick={handleLoadButton}>Carregar categorias</button>
       Total de categorias: {categories.length}
+
+      <div className="grid grid-cols-5">
+        {categories.map((item) => (
+          <div>
+            <p>{item.name}</p>
+            <img src={item.img} className="w-10" />
+          </div>
+        ))}
+      </div>
 
     </div>
   );
