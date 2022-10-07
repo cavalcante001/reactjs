e37302d909075825781d31c5f70eee683ab17d6a
Paulo <paulocavalcante001@outlook.com>
2022-10-07 17:18:40 -0300

    lidando com loading

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,40 +1,58 @@
 import { useState, useEffect } from "react";
-import { Category } from './types/Category'
+import { Category } from "./types/Category";
 
 const App = () => {
   const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     handleLoadButton();
   }, []);
 
-  const handleLoadButton = () => {
-    fetch('http://localhost.com:5003/categories')
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        setCategories(json.categories);
-      })
+  // const handleLoadButton = () => {
+  //   fetch('http://localhost.com:5003/categories')
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((json) => {
+  //       setCategories(json.categories);
+  //     })
 
-  }
-    
+  // }
+
+  const handleLoadButton = async () => {
+    setLoading(true);
+    const req = await fetch("http://localhost.com:5003/categories");
+    const json = await req.json();
+    setCategories(json.categories);
+    setLoading(false);
+  };
+
   return (
     <div>
-      <button className="block bg-blue-400 p-2 rounded" onClick={handleLoadButton}>Carregar categorias</button>
-      Total de categorias: {categories.length}
+      {!loading && (
+        <>
+          <button
+            className="block bg-blue-400 p-2 rounded"
+            onClick={handleLoadButton}
+          >
+            Carregar categorias
+          </button>
+          <div>Total de categorias: {categories.length}</div>
 
-      <div className="grid grid-cols-5">
-        {categories.map((item) => (
-          <div>
-            <p>{item.name}</p>
-            <img src={item.img} className="w-10" />
+          <div className="grid grid-cols-5">
+            {categories.map((item) => (
+              <div>
+                <p>{item.name}</p>
+                <img src={item.img} className="w-10" />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-
+        </>
+      )}
+      {loading && <div>Carregando...</div>}
     </div>
   );
-}
+};
 
 export default App;
