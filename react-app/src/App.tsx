e2a92a2c0f60910bbf10b0c3e0ca3a0ed8f674b3
Paulo <paulocavalcante001@outlook.com>
2022-10-07 20:28:00 -0300

    reducer

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,45 +1,22 @@
-import { useState, useEffect } from "react";
-import { Category } from "./types/Category";
-import { PostForm } from "./components/PostForm";
-import { PostItem } from "./components/PostItem";
-import { api } from "./api";
+import { useReducer } from "react";
+import { useContagem } from "./reducers/contagem";
 
 const App = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    handleLoadButton();
-  }, []);
-
-  const handleLoadButton = async () => {
-    setLoading(true);
-    let json = await api.getCategories();
-    setCategories(json);
-    setLoading(false);
-  };
-
-  const handleToken = async (email: string, password: string) => {
-    const json = await api.postToken(email, password);
-    console.log(json);
-  };
+  const [contagem, dispatch] = useContagem();
 
   return (
-    <div>
-      {loading && <div>Carregando...</div>}
-
-      <PostForm onAdd={handleToken} />
-
-      {!loading && categories.length === 0 && <div>Tente mais tarde...</div>}
-
-      {!loading && categories.length > 0 && (
-        <>
-          <div>Total de categorias: {categories.length}</div>
-          <br />
-
-          <PostItem data={categories} />
-        </>
-      )}
+    <div className="p-5">
+      Contagem: {contagem.count}
+      <hr />
+      <button className="p-3" onClick={() => dispatch({ type: "ADD" })}>
+        Adicionar
+      </button>
+      <button className="p-3" onClick={() => dispatch({ type: "DEL" })}>
+        Remover
+      </button>
+      <button className="p-3" onClick={() => dispatch({ type: "RESET" })}>
+        Resetar
+      </button>
     </div>
   );
 };
